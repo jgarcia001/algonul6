8308937b474fcec39481e956e7e1fc064cd38022
polochinoc <polochinoc@gmail.com>
2017-05-19 12:14:22 +0200

    i don't know what to write

--- methodes.py
+++ methodes.py
@@ -31,23 +31,22 @@ def step_runge_kutta_4(y, t, h, f):
 def meth_n_step(y0, t0, N, h, f, meth):
     start_y = y0
     start_x = t0
-    Y = [start_y]
-    for i in range(N):
+    Y = np.array([start_y])
+    for i in range(1,N):
         start_y = meth(start_y, start_x, h, f)
         start_x += h
-        Y.append(start_y)
+        Y = np.append(Y, start_y)
     return Y
 
-print(meth_n_step(0., 1., 100, 0.01, lambda y,t : y/(1 + t**2), step_euler))
-
-def meth__epsilon(y0, t0, tf, esp, f, meth):
+def meth__epsilon(y0, t0, tf, eps, f, meth):
     start_y = y0
     start_x = t0
-    Y = [start_y]
-    while (nl.norm(meth(start_y, start_x, h, f) - f(start_y, start_x)) > eps):
+    h = (tf - t0) * eps
+    Y = np.array([start_y])
+    while (start_x < tf):
         start_y = meth(start_y, start_x, h, f)
         start_x += h
-        Y.append(start_y)
+        Y = np.append(Y, start_y)
     return Y
 
 #==============TANGENTS===============#
@@ -70,13 +69,50 @@ def tangents_field(y0, t0, h, f, meth, N):
     plt.show()
     return
 
-#tangents_field(1, 0, 0.01, lambda y,t : y/(1 + t**2), step_euler, 50)
+#tangents_field(1., 0., 0.01, lambda y,t : y/(1 + t**2), step_euler, 50)
 
 #==============TEST_ZONE==============#
 
 def test_methodes():
     # Dimension 1
-    t0 = 0
-    y0 = 1
+    t0 = 0.
+    y0 = 1.
+    N = 10000
+    h = 0.01
     f = lambda y,t : y/(1 + t**2)
+
+    #print(meth_n_step(y0, t0, N, h, f, step_euler))
+    #print(meth_n_step(y0, t0, N, h, f, step_middle_point))
+    #print(meth_n_step(y0, t0, N, h, f, step_heun))
+    #print(meth_n_step(y0, t0, N, h, f, step_runge_kutta_4))
+
+    # Plot Section
+
+    X = np.arange(t0, t0 + (N)*h, h)
+    Y_euler = meth_n_step(y0, t0, N, h, f, step_euler)
+    Y_middle_point = meth_n_step(y0, t0, N, h, f, step_middle_point)
+    Y_heun = meth_n_step(y0, t0, N, h, f, step_heun)
+    Y_runge_kutta_4 = meth_n_step(y0, t0, N, h, f, step_runge_kutta_4)
+
+    plt.plot(X, Y_euler, label='Euler')
+    plt.plot(X, Y_middle_point, label='Middle point')
+    plt.plot(X, Y_heun, label='Heun')
+    plt.plot(X, Y_runge_kutta_4, label='Runge-Kutta 4')
+    plt.legend()
+    plt.show()
+
+    tf = 3.
+
+    #print(meth__epsilon(y0, t0, tf, 1/N, f, step_euler))
+    #print(meth__epsilon(y0, t0, tf, 1/N, f, step_middle_point))
+    #print(meth__epsilon(y0, t0, tf, 1/N, f, step_heun))
+    #print(meth__epsilon(y0, t0, tf, 1/N, f, step_runge_kutta_4))
+
+    # Dimension 2
+    t0 = 0.
+    y0 = np.array([1, 0])
+    N = 100
+    h = 0.01
+    f = lambda y,t : np.array([-y[1], y[0]])
+
     return
