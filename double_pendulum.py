00a71bb161072504fb04a8f4a3674567d6ac1480
timarr <arroyo.timothe@gmail.com>
2017-05-28 15:49:51 +0200

    ajout de commentaire

--- double_pendulum.py
+++ double_pendulum.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import methodes as m
 
-#----------------------------------------------------------------------------
+#--------------------------------------------------------------------- 
+#Constantes utilisees lors des tests
 
 g = 9.81
 N = 500
@@ -15,20 +16,25 @@ l = 1.0
 t1 =-3
 t2 =3
 
-
-#----------------------------------------------------------------------------
-
+#--------------------------------------------------------------------- 
+#Fonctions implementees pour le pendule a deux maillons                
+                                                                       
+#Retourne la position (x,y) du premier solide
 def pos1(l_1, theta):
     x1 = l_1 * np.sin(theta)
     y1 = -l_1 * np.cos(theta)
     return x1, y1
 
+#Retourne la position (x,y) du deuxième solide
 def pos2(l_1, l_2, theta1, theta2):
     x1, y1 = pos1(l_1, theta1)
     x2 = x1 + l_2 * np.sin(theta2)
     y2 = y1 - l_2 * np.cos(theta2)
     return x2, y2
 
+#Retourne sous la forme d'une lambda-expression un tableau contenant:  
+# -les vitesses des solides                                            
+# -les accélérations des solides
 def f_function():
 
     return (lambda u,t: np.array([u[1],
@@ -43,17 +49,20 @@ def f_function():
                                                           + g*2*np.cos(u[0]) 
                                                           + (u[3]**2)*l*np.cos(u[0] - u[2])))
                                   / (l*(3 - np.cos(2*(u[0] - u[2]))))]))
-    
+#Retourne les solutions de l'équation du mouvement en fonction         
+# des conditions initiales (y_zero)    
 def double_pendulum_position_function(y_zero):
 
     f = f_function()
     return m.meth_n_step(y_zero, 0, N, h, f, m.step_runge_kutta_4)
 
-#----------------------------------------------------------------------------
-
-def find_value_x(y0, y1, i, value):
-    return ((value-y0)/(y1 - y0 ))+(i-1)
-
+#--------------------------------------------------------------------- 
+#Tests realises pour tester les fonctions precedentes                  
+                                                                       
+#Retourne les solutions de l'équation du mouvement avec :              
+# -un tableau pour l'angle theta1                                      
+# -un tableau pour l'angle theta2                                      
+# -le temps du premier retournement
 def double_pendulum(t1, t2):
     u_init = np.array([t1, 0, t2, 0])
     tab = double_pendulum_position_function(u_init)
@@ -77,14 +86,17 @@ def double_pendulum(t1, t2):
     return the1, the2, first_flip
 
 
-
+#Affiche le grahique de l'angle du deuxième solide en fonction         
+#de celui du premier solide. 
 def test_modelling():
     the1, the2, first_flip = double_pendulum(t1, t2)
     plt.plot(the1, the2)
     plt.show()
 
 test_modelling()
-    
+
+#Affiche le graphique de la position du deuxième pendule lors          
+# de la simulation: y en fonction de x
 def test_modelling_position():
     the1, the2, firstflip = double_pendulum(t1, t2)
 
@@ -103,6 +115,8 @@ def test_modelling_position():
 
 test_modelling_position()
 
+#Affiche la carte des premiers retournements suivant les conditions in\
+itiales
 def test_flip():
     N = 200
     h = 0.05
